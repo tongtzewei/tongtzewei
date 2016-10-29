16d5a8e90198bbec5e207da96afeb0353935d95f
Tong Tze Wei <tongtzewei@outlook.com>
2016-10-29 16:46:20 +0800

    use original code

--- app.js
+++ app.js
@@ -1,70 +1,147 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var builder = require('botbuilder');
+
+var restify = require('restify');
+
+
 
 // Setup Restify Server
+
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
-   console.log('%s listening to %s', server.name, server.url); 
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+
+    console.log('%s listening to %s', server.name, server.url);
+
 });
 
+
+
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: '0b6b5b0d-a8c6-4759-ab0a-de2241bf04ac', appPassword: 'PN4xsTqsBBgCS1bLcmtA824' }); 
+
+var connector = new builder.ChatConnector({
+
+    appId: '0b6b5b0d-a8c6-4759-ab0a-de2241bf04ac',
+
+    appPassword: 'PN4xsTqsBBgCS1bLcmtA824'
+
+});
+
 var bot = new builder.UniversalBot(connector);
+
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
-var Hotels = require('./hotels.js');
-var Flights = require ('./flights.js');
-var Support = require ('./support.js');
+
+
+// Dialogs
+
+var Hotels = require('./hotels');
+
+var Flights = require('./flights');
+
+var Support = require('./support');
+
+
+
+// Setup dialogs
 
 bot.dialog('/flights', Flights.Dialog);
-bot.dialog('/hotels', Hotels.Dailog);
+
+bot.dialog('/hotels', Hotels.Dialog);
+
 bot.dialog('/support', Support.Dialog);
 
+
+
+// Root dialog
+
 bot.dialog('/', new builder.IntentDialog()
-    .matchesAny([/help/i, /support/i, /problem/i],[
-        function(session){
+
+    .matchesAny([/help/i, /support/i, /problem/i], [
+
+        function (session) {
+
             session.beginDialog('/support');
+
         },
-        function(session, result){
-            var ticketNumber = result.response;
-            session.send('Thanks for contacting our support team. Your ticket number is %s',ticketNumber);
+
+        function (session, result) {
+
+            var tickerNumber = result.response;
+
+            session.send('Thanks for contacting our support team. Your ticket number is %s.', tickerNumber);
+
             session.endDialog();
+
         }
+
     ])
+
     .onDefault([
-        function(session){
+
+        function (session) {
+
+            // prompt for search option
+
             builder.Prompts.choice(
-                session, 
+
+                session,
+
                 'Are you looking for a flight or a hotel?',
-//                [Flights.label, Hotels.label],
-                    ["Hotels", "Flights"],
+
+                [Flights.Label, Hotels.Label],
+
                 {
-                    maxRetries : 3,
+
+                    maxRetries: 3,
+
                     retryPrompt: 'Not a valid option'
+
                 });
+
         },
-        function(session, result){
-            if (!result.response){
-                session.send('Ooopgs! too many attempt');
+
+        function (session, result) {
+
+            if (!result.response) {
+
+                // exhausted attemps and no selection, start over
+
+                session.send('Ooops! Too many attemps :( But don\'t worry, I\'m handling that exception and you can try again!');
+
                 return session.endDialog();
+
             }
-        
-            session.on('error',function(err){
-            session.send('Failed with messsage %s', err.message);
-            session.endDialog();
+
+
+
+            // on error, start over
+
+            session.on('error', function (err) {
+
+                session.send('Failed with message: %s', err.message);
+
+                session.endDialog();
+
             });
 
+
+
+            // continue on proper dialog
+
             var selection = result.response.entity;
-            switch(selection){
- //               case Fiights.label: return session.beginDialog('/flights')
-                  case "Flights": return session.beginDialog('/flights')
- //               case Hotels.Label: return session.beginDialog('/hotels')
-                  case "Hotels" : return session.beginDialog('/hotels')
+
+            switch (selection) {
+
+                case Flights.Label:
+
+                    return session.beginDialog('/flights')
+
+                case Hotels.Label:
+
+                    return session.beginDialog('/hotels')
+
             }
+
         }
+
     ]));
-    
